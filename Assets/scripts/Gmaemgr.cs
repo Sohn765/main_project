895a1752dbfa1f62aae8328a2aaec1e6981d0050
Sohn765 <113961786+Sohn765@users.noreply.github.com>
2022-10-30 20:58:52 +0900

    종소리 추가

--- Assets/scripts/Gmaemgr.cs
+++ Assets/scripts/Gmaemgr.cs
@@ -8,32 +8,36 @@ public class Gmaemgr : MonoBehaviour
     public Text talkText;
     public OP_playerMove move;
     public GameObject zz;
-    public int clickcount;
-    public int speed;
-    public AudioSource clicksound;
+    public int clickcount,playSound, speed;
+    public AudioSource bellsound,clicksound;
     void Start()
     {
         clickcount = 0;
+        playSound = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!bellsound.isPlaying && playSound == 0)
+        {
+            setactive();
+        }
+    }
+    void setactive()
+    {
 
+        zz.SetActive(true);
+        talkText.text = "아... 내일 점심은 먹을 게 없네 자판기나 가야겠다";
+        clickcount++;
+        playSound++;
     }
     private void OnMouseDown()
     {
-        
-        if(clickcount == 0)
-        {
-            zz.SetActive(true);
-            talkText.text = "아... 내일 점심은 먹을 게 없네 자판기나 가야겠다";
-            clickcount++;
-        }
-        
 
-        else if(clickcount == 1)
+        if (clickcount == 1)
         {
+            clicksound.Play();
             zz.SetActive(false);
             move.moveY = -1;
         }
