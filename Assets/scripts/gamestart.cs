81d088897398a1de1d226b56946e97d7316f03e2
chldPDms <115327796+chldPDms@users.noreply.github.com>
2022-11-02 02:48:49 +0900

    음악실 수정본입니다?
    감사합니다?

--- Assets/scripts/gamestart.cs
+++ Assets/scripts/gamestart.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playgame : MonoBehaviour
+public class gamestart : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -15,10 +15,4 @@ public class playgame : MonoBehaviour
     {
         
     }
-    public void gameplay()
-    {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(7);
-
-    }
 }
- 
