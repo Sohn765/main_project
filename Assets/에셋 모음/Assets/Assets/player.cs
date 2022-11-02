0f51d405e81967b07ec86c65ad7cdc1f513f11d2
Sohn765 <113961786+Sohn765@users.noreply.github.com>
2022-11-03 03:03:23 +0900

    음악게임 에셋

--- Assets/에셋 모음/Assets/Assets/player.cs
+++ Assets/에셋 모음/Assets/Assets/player.cs
@@ -16,6 +16,7 @@ public class player : MonoBehaviour
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+        Time.timeScale = 1;
     }
 
     // Update is called once per frame
