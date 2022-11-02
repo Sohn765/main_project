95467de7104d1f0b64b77dd1098cd24d6a6cb1d3
Sohn765 <113961786+Sohn765@users.noreply.github.com>
2022-11-03 02:22:29 +0900

    Revert "Revert "코딩게임 2-2 끝""
    This reverts commit daa33cb9c587ebedd52de9721e0c7c27c3313ef9.

--- Assets/scripts/treasure.cs
+++ Assets/scripts/treasure.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class jumpCount : MonoBehaviour
+public class treasure : MonoBehaviour
 {
-    public player player;
+    public GameObject coin;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,15 +16,12 @@ public class jumpCount : MonoBehaviour
     {
         
     }
+
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if(collision.gameObject.tag== "player")
+        if (Input.GetKey(KeyCode.E))
         {
-            player.incount = 1;
+            coin.SetActive(true);
         }
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        player.incount =0;
     }
 }
