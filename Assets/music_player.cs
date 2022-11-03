8c9396707b71b0e9c0bd4af14d41cb8a61922574
Sohn765 <113961786+Sohn765@users.noreply.github.com>
2022-11-03 23:00:23 +0900

    점프 모션

--- Assets/music_player.cs
+++ Assets/music_player.cs
@@ -5,26 +5,45 @@ using UnityEngine.UI;
 
 public class music_player : MonoBehaviour
 {
-    float moveX, moveY;
+    public float moveX, moveY = 0, jumpcount;
     public float jumpPower;
     Rigidbody2D rb;
+    Animator an;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        an = gameObject.GetComponent<Animator>();   
     }
 
     // Update is called once per frame
     void Update()
     {
         moveX = Input.GetAxisRaw("Horizontal");
-        moveY = Input.GetAxisRaw("Vertical");
-        rb.AddForce(Vector2.right * moveX, ForceMode2D.Impulse);
+        an.SetFloat("jump", Mathf.Abs(moveY));
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && jumpcount == 1)
+        {
+            moveY = 7;
+            rb.AddForce(Vector2.up * moveY, ForceMode2D.Impulse);
+            jumpcount = 0;
+        }
+        if (rb.velocity.y < 0)
+        {
+            Debug.DrawRay(rb.position, Vector3.down, new Color(0, 1, 0));
+            RaycastHit2D rayHit = Physics2D.Raycast(rb.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+            if (rayHit.collider != null)
+            {
+                if (rayHit.distance < 0.3f)
+                {
+                    jumpcount = 1;
+                    moveY = 0;
+                }
+            }
+        }
+
     }
 
-    void jump()
-    {
-        if (Input.GetKey(KeyCode.Space)) ;
-    }
 }
