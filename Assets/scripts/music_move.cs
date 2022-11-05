46e1ca3d1b926d82a338f3bf1ddc5169b1a074c0
chldPDms <115327796+chldPDms@users.noreply.github.com>
2022-11-06 03:19:40 +0900

    수정 5

--- Assets/scripts/music_move.cs
+++ Assets/scripts/music_move.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class music_move : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Vector2 target = new Vector2(5, 10);
+    Vector2 target1 = new Vector2(-5, 10);
+    
+    private void Start()
     {
-        
+       
     }
-
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector2.MoveTowards(transform.position, target, 0.1f);
+        StartCoroutine("moving");
+       
     }
+
+  
+
+
 }
